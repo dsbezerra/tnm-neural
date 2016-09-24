7c8b10908257b91825d93b17c02c44acf9bea085
Diego Bezerra <Diego Bezerra>
2016-09-24 20:40:34 -0300

    fixed no package found

--- tnm_neural/settings.py
+++ tnm_neural/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tnm_neural.category_classifier'
+    'category_classifier'
 ]
 
 MIDDLEWARE = [

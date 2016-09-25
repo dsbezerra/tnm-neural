b781d2a941a9066688f55d71a29348f82b9a4e04
Diego Bezerra <Diego Bezerra>
2016-09-24 22:44:14 -0300

    Fix url process

--- category_classifier/urls.py
+++ category_classifier/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'process', views.process, name='process')
+    url(r'process$', views.process, name='process')
 ]

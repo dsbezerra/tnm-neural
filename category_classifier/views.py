57c0a8e571125e9913c4cd2e3ede27f971a73cce
Diego Bezerra <Diego Bezerra>
2016-09-24 22:40:29 -0300

    Refactoring

--- category_classifier/views.py
+++ category_classifier/views.py
@@ -15,26 +15,23 @@ def index(request):
 def process(request):
     if request.method != "POST":
         return HttpResponse()
-    else:
-
-        print request.body
-
-        if request.body and request.content_type == 'application/json':
-            json_body = json.loads(request.body)
-            category_classifier = CategoryClassifier(json_body['input'])
-            results = category_classifier.run()
-
-            print results
-
-            if results:
-                # TODO(diego): Use database to save predicted values and to control duplication
-                # If array with more than one record, use Bulk from mongo to perform an efficient
-                # insert into database, otherwise just use a normal insert operation. 
-                
-                # For now just send the information back to client
-
-                return JsonResponse(results)
-            else:
-                return HttpResponse()
+    
+    if request.body:
+        json_body = json.loads(request.body)
+        category_classifier = CategoryClassifier(json_body['input'])
+        results = category_classifier.run()
+        
+        print results
+        
+        if results:
+            # TODO(diego): Use database to save predicted values and to control duplication
+            # If array with more than one record, use Bulk from mongo to perform an efficient
+            # insert into database, otherwise just use a normal insert operation. 
+            
+            # For now just send the information back to client
+            
+            return JsonResponse(results)
         else:
             return HttpResponse()
+    else:
+        return HttpResponse()
